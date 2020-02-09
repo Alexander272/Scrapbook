5eba3f3108605d0ca2f2357e0190bfa08644e631
Alexander272 <46869920+Alexander272@users.noreply.github.com>
2020-02-09 10:26:28 +0500

    Update keys.prod.js

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     MONGODB_URL: 'mongodb+srv://Alex:2fQd48Ea5if7KRgL@cluster0-gexu4.mongodb.net/photoAlbums',
-    SESSION_SECRET: process.env.SESSION_SECRET,
+    SESSION_SECRET: 'some secret value',
     BASE_URL: process.env.BASE_URL,
     CLARIFAI_KEY: '64484726be7f439297f3fd5b91d0d78c'
 }
