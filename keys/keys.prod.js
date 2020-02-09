592a7a63f89ca2630c91242ceda389615feec6ff
Alexander <martynovalexander272@gmail.com>
2020-02-09 10:06:04 +0500

    new

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-    MONGODB_URL: process.env.MONGODB_URL,
+    MONGODB_URL: 'mongodb+srv://Alex:2fQd48Ea5if7KRgL@cluster0-gexu4.mongodb.net/photoAlbums',
     SESSION_SECRET: process.env.SESSION_SECRET,
     BASE_URL: process.env.BASE_URL,
-    CLARIFAI_KEY: process.env.CLARIFAI_KEY
+    CLARIFAI_KEY: '64484726be7f439297f3fd5b91d0d78c'
 }
